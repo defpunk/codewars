f1c885e3233fa63a11d483cc5087acbc8d8d66af
David Oliver <david.oliver@qwickr.com>
2024-09-03 13:46:09 +0100

    added initial csharp version

---
+++ csharp/CodeWars.Tests/ProgramTests.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using CodeWars;
+
+namespace CodeWars.Tests
+{
+        public class IsEvenTests
+        {
+                [Fact]
+                public void IsEvenReturnsTrueForEvenNumber()
+                {
+                        var number = 10;
+                        var expectedResult = true;
+                        var result = Program.IsEven(number);
+                        Assert.Equal(expectedResult, result);
+                }
+
+                [Fact]
+                public void IsEvenReturnsFalseForOddNumber()
+                {
+                        var number = 11;
+                        var expectedResult = false;
+                        var result = Program.IsEven(number);
+                        Assert.Equal(expectedResult, result);
+                }
+        }
+}
---
+++ csharp/CodeWars/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeWars
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 8;
+            bool result = IsEven(number);
+
+            Console.WriteLine($"Is {number} even? {result}");
+        }
+
+        public static bool IsEven(int number)
+        {
+            return (number % 2) == 0;
+        }
+    }
+}
